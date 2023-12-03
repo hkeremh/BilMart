505a889b51eb86e3d9cfc988054b9614ad41f1f1
mert.fidan715 <mert.fidan@ug.bilkent.edu.tr>
2023-12-03 18:13:47 +0300

    style fixes

--- Bilmart MVC/view/src/components/create.jsx
+++ Bilmart MVC/view/src/components/create.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router";
 import { useCookies } from "react-cookie";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/esm/Button.js"; 
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 import NavBar from "./navbar.jsx";
@@ -265,6 +266,101 @@ export default function Create() {
       </form>
       <ToastContainer />
     </div>
+    <div>
+    <NavBar />
+    <div style={{ marginTop: 15 }}>
+    <div class="container" style={{marginTop: "5rem"}}>
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style={{borderRadius: "25px"}}>
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 text">Post a new listing</p>
+                <form class="mx-1 mx-md-4" onSubmit={onSubmit}>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-lock-fill fa-lg me-3 fa-fw" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2"/>
+                      </svg>
+                      <div class="form-outline flex-fill mb-0">
+                        <label className="form-label fw-bold text" htmlFor="password">Title</label>
+                        <input
+                          className="form-control text"
+                          type="title"
+                          name="title"
+                          value={form.title}
+                          placeholder="Enter title"
+                          onChange={(e) => updateForm({ title: e.target.value })}
+                        />
+                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-lock-fill fa-lg me-3 fa-fw" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2"/>
+                      </svg>
+                      <div class="form-outline flex-fill mb-0">
+                        <label className="form-label fw-bold text" htmlFor="description">Description</label>
+                        <input
+                          className="form-control text"
+                          type="description"
+                          name="description"
+                          value={form.description}
+                          placeholder="Enter description"
+                          onChange={(e) => updateForm({ description: e.target.value })}
+                        />
+                      </div>
+                  </div>
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-image-fill fa-lg me-3 fa-fw" viewBox="0 0 16 16">
+                        <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
+                    </svg>
+                    <div class="form-outline flex-fill mb-0">
+                      <label className="form-label fw-bold text" htmlFor="password">Upload Pictures</label>
+                      <input
+                        className="form-control text"
+                        type="file"
+                        id="image"
+                        accept="image/*"
+                        onChange={(e) => {
+                        let file = e.target.files[0];
+                        let reader = new FileReader();
+                        reader.onloadend = function () {
+                            // compressImage(reader.result, 0.1, (compress) => {
+                            //     updatePhoto(compress);
+                            // })
+                            updateSources(reader.result);
+                        }
+                        reader.readAsDataURL(file);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="submit" value="Update Profile" className="btn btn-dark"><span className="text">Update Profile</span></button>
+                  </div>
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <span className="text">
+                        <Button variant="outline-danger" href="/profile">Cancel</Button>
+                    </span>
+                  </div>
+                </form>
+              </div>
+              <div class="justify-content-center col-md-10 col-lg-6 col-xl-7 align-items-center order-1 order-lg-2">
+                <p class="text-center h2 fw-bold mb-5 mx-1 mx-md-4 mt-4 text">Selected Pictures:</p>
+                <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>{sources.map((source) => {
+                  return <img className="centered-and-cropped" width={source.width * (100 / source.height)} height="200" style={{borderRadius: "5%", margin: "10px"}} src={source} />
+                })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <ToastContainer />
+    </div>
+  </div>
     </div>
   );
 }
