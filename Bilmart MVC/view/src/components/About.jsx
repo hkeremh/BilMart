e6d2c4a4817447d46b5d23dff997d7eb1c866a94
Mert Fidan <mert.fidan@ug.bilkent.edu.tr>
2023-12-17 17:10:05 +0300

    spelling fix

--- Bilmart MVC/view/src/components/About.jsx
+++ Bilmart MVC/view/src/components/About.jsx
@@ -32,7 +32,7 @@ function About(){
             <h3 className="fs-2 text-body-emphasis text" style={{color: "black", fontWeight: "bolder"}}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="var(--text-color3)" class="bi bi-shield-lock-fill" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"/>
             </svg> Secure</h3>
-            <p className="text" style={{color: "black"}}>BilMart prioritizes cutting-edge security measures to ensure a safe environment. Employing robust encryption protocols and regular security audits, BilMart fosters a security. This unwavering commitment to security establishes BilMart as a trusted and reliable destination.</p>
+            <p className="text" style={{color: "black"}}>BilMart prioritizes cutting-edge security measures to ensure a safe environment. Employing robust encryption protocols and regular security audits, BilMart fosters security. This unwavering commitment to security establishes BilMart as a trusted and reliable destination.</p>
             <a href="/signup" className="icon-link">
               Get started with BilMart
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
